d6c4939a61bae6bca7f9213ac1fcb71a1e3edf55
YZH <yuzh0711@foxmail.com>
2020-08-16 09:21:36 +0800

    9点半

---
+++ Assets/SpawnDialog.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnDialog : MonoBehaviour
+{
+    [SerializeField] List<GameObject> dialogs;
+    private void Start()
+    {
+        StartCoroutine(Spwan());
+    }
+
+    IEnumerator Spwan()
+    {
+        while (true)
+        {
+            int num = UnityEngine.Random.Range(0, dialogs.Count);
+            GameObject temp = Instantiate(dialogs[num],new Vector3(transform.position.x+Random.Range(-1,1f), transform.position.y + Random.Range(-1, 1f)), Quaternion.identity);
+           
+            yield return new WaitForSeconds(4);
+        }
+    }
+}
